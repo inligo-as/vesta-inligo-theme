58993a8e384717de165b9df056d7609b9f593a61
Joakim Frogner <joakim@inligo.no>
2020-10-19 15:04:15 +0200

    add more links to sidebar

--- web/functions.php
+++ web/functions.php
@@ -1,4 +1,5 @@
 <?php
 
 Vesta::add_css('/plugin/vesta-inligo-theme/style.css', 8);
+Vesta::add_menu('Inligo Tools', '/plugin/vesta-inligo-apps/', 'Inligo Tools');
 Vesta::add_menu('Server', '/list/server/', 'Server');
